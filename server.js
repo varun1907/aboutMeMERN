258dfb3d4141ef6f7db2ce8f00ee204e47b753b2
varun1907 <varun1907@gmail.com>
2018-11-01 02:22:04 +0530

    production deployable changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const queries = require('./routes/api/queries');
 const db = require('./config.js');
+const path = require('path');
 
 const app = express();
 
@@ -16,6 +17,14 @@ mongoose.connect(db.mongoURI)
 
 // Use routes
 app.use('/api/queries', queries);
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server started on port ${port}`));
