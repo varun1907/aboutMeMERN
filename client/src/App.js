9e29014a80daebd8bc207fb3b73108f6e2c0195a
varun1907 <varun1907@gmail.com>
2018-11-16 19:32:46 +0530

    camera testing

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Skills from './components/Skills.js';
 import Projects from './components/Projects.js';
 import Contacts from './components/Contacts.js';
 import IndividualProject from './components/IndividualProject';
+import Cameras from './components/Cameras';
 import './App.css';
 
 
@@ -21,6 +22,8 @@ class App extends Component {
       <Route exact path="/projects" component={Projects}/>
       <Route exact path="/projects/:id" component={IndividualProject}/>
       <Route exact path="/contacts" component={Contacts}/>
+      <Route exact path="/camera" component={Cameras}/>
+
     </div>
 
   </Router>
